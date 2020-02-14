be69b0a2f6c6e6b0ce4ae2cbfea5074bd519aecd
이창섭 <ventulus95@gmail.com>
2020-02-14 17:40:58 +0900

    첫 커밋

---
+++ src/test/java/com/ventulus95/boardproject/BoardprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventulus95.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
